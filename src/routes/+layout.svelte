39ae4b7fba85a6ffc26856470e7f0947b14b37ae
Vlad Glivuk <vlad.glivuk@gmail.com>
2024-05-29 14:52:51 +0300

    add layout and pico library

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from "$lib/components/header.svelte";
+  import Footer from "$lib/components/footer.svelte";
+  import '@picocss/pico';
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<Footer />
