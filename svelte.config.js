5182601891f45e9ce0a1af32f6120dfa236bd461
Vlad Glivuk <vlad.glivuk@gmail.com>
2024-06-09 18:23:50 +0300

    fix KV configuration

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-				config: './wrangler.toml',
-				platformProxy: {
-					persist: './kv-storage'
-				}
+			config: './wrangler.toml',
+			platformProxy: {
+				configPath: './wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
 		})
 	}
 };
